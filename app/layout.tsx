3dd280a4bdaeae664c13609379a3682376da77d1
Kanad <codewizardkanad@gmail.com>
2025-06-27 21:24:08 +0530

    Fixed some bugs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -25,6 +26,7 @@ export default function RootLayout({
       >
         <div className="relative max-w-7xl mx-auto bg-[#1a1922] px-4 md:px-8">
           {children}
+          <Analytics />
         </div>
       </body>
     </html>
